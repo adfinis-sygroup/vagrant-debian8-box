9753b41a1668ede3f93268066009fa4721cf0bd2
Jean-Louis Fuchs <ganwell@fangorn.ch>
2015-05-14 16:15:37 +0200

    Tweaking image size even more

--- definitions/adsy-debian-8.0.0/cleanup.sh
+++ definitions/adsy-debian-8.0.0/cleanup.sh
@@ -1,5 +1,8 @@
 # Clean up
 aptitude -y purge linux-headers-$(uname -r)
+aptitude -y purge task-laptop laptop-detect
+dpkg --list | grep "^rc" | cut -d " " -f 3 | xargs sudo dpkg --purge
+aptitude install -f -y
 apt-get -y autoremove
 apt-get -y clean
 
@@ -16,3 +19,16 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
+
+echo "Remove documentation"
+find /usr/share/doc -depth -type f ! -name copyright|xargs rm || true
+find /usr/share/doc -empty|xargs rmdir || true
+
+cat > /etc/dpkg/dpkg.cfg.d/01_nodoc <<EOF
+path-exclude /usr/share/doc/*
+# we need to keep copyright files for legal reasons
+path-include /usr/share/doc/*/copyright
+EOF
+
+echo "Remove caches"
+rm -r /var/cache/apt /var/lib/apt/lists
